012840d92ddaf32128e5b935df7a9940278505f0
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-08-23 15:39:16 +0000

    add inline astro config to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,19 @@
 
 import { getViteConfig } from "astro/config";
 
-export default getViteConfig({
-  test: {
-    clearMocks: true,
-    mockReset: true,
-    restoreMocks: true,
-    silent: "passed-only",
-    unstubEnvs: true,
-    unstubGlobals: true,
+export default getViteConfig(
+  {
+    test: {
+      clearMocks: true,
+      mockReset: true,
+      restoreMocks: true,
+      silent: "passed-only",
+      unstubEnvs: true,
+      unstubGlobals: true,
+    },
   },
-});
+  {
+    site: "https://cturner8.github.io",
+    trailingSlash: "always",
+  },
+);
